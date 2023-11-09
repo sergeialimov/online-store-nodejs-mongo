7eb1fa9734582be7c1f4a220fea9f737c8e8ff9c
Sergei <dev.sergeialimov@gmail.com>
2023-11-09 15:53:58 +0300

    update faker calling

--- apps/app/faker.util.ts
+++ apps/app/faker.util.ts
@@ -1,21 +1,25 @@
 import { faker } from '@faker-js/faker';
+import { Customer } from '../../libs/db';
 
 function createRandomCustomer(): Customer {
   return {
-    firstName: faker.person.firstName;
-    firstName: faker.person.lastName;
-    email: faker.internet.email;
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
     address: {
-      line1: faker.location.streetAddress,
-      line2: faker.location.streetAddress,
-      postcode: faker.location.zipCode,
-      city: faker.location.city,
-      state: faker.location.state,
-      country: faker.location.country,
+      line1: faker.location.streetAddress(),
+      line2: faker.location.secondaryAddress(),
+      postcode: faker.location.zipCode(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      country: faker.location.country(),
     }
   }
 }
 
-export const Customers: Customer[] = faker.helpers.multiple(createRandomCustomer, {
-  count: 5,
-});
+export function getCustomers (amount: number): Customer[] {
+  return faker.helpers.multiple(
+    createRandomCustomer,
+    { count: amount }
+  );
+}
