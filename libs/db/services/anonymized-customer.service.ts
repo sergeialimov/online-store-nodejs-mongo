0e7f31480f22cd3a12ad123113fada701ebdb80d
Sergei <dev.sergeialimov@gmail.com>
2023-11-13 16:29:59 +0300

    anonymized-customer.service

---
+++ libs/db/services/anonymized-customer.service.ts
@@ -0,0 +1,18 @@
+import { MongoClient, Collection } from 'mongodb';
+import { AnonymizedCustomer } from './anonymized-customer.model';
+
+export class AnonymizedCustomerService {
+  private dbClient: MongoClient;
+  private collection: Collection<AnonymizedCustomer>;
+
+  constructor(dbClient: MongoClient) {
+    this.dbClient = dbClient;
+    this.collection = this.dbClient.db().collection('customers_anonymized');
+  }
+
+  public async insertBatch(batch: AnonymizedCustomer[]): Promise<void> {
+    if (batch.length > 0) {
+      await this.collection.insertMany(batch);
+    }
+  }
+}
