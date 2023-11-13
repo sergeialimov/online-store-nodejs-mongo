5a0a4805d557e1c79b6228719239237d232c2e78
Sergei <dev.sergeialimov@gmail.com>
2023-11-13 16:30:35 +0300

    update sync.ts

--- apps/sync/sync.ts
+++ apps/sync/sync.ts
@@ -1,14 +1,48 @@
 import "dotenv/config";
-import { connectToDatabase, CustomerService } from "../../libs/db";
+import {
+  connectToDatabase,
+  AnonymizedCustomer
+  CustomerService,
+  AnonymizedCustomerService,
+} from "../../libs/db";
+
+import { saveResumeToken, getResumeToken } from "../../apps/";
 
 (async () => {
   const client = await connectToDatabase();
   const customerService = new CustomerService(client);
-
+  const anonymizedCustomerService = new AnonymizedCustomerService(client);
   const changeStream = customerService.getChangeStream();
+  let batch: AnonymizedCustomer[] = [];
+  let batchTimer: NodeJS.Timeout | null = null;
 
-  console.log("Listening for changes to the customers collection...");
-  changeStream.on("change", (change) => {
-    console.log("Change detected:", change);
-  });
+  try {
+    const lastToken = await getResumeToken();
+    if (lastToken) {
+      changeStream.resumeAfter(lastToken);
+    }
+
+    for await (const change of changeStream) {
+      const resumeToken = change._id;
+      const anonymizedData = anonymizeCustomer(change.fullDocument);
+      batch.push(anonymizedData);
+
+      if (batch.length === 1000) {
+        await anonymizedCustomerService.insertBatch(batch);
+        batch = [];
+        if (batchTimer) clearTimeout(batchTimer);
+        await saveResumeToken(resumeToken);
+      } else if (!batchTimer) {
+        batchTimer = setTimeout(async () => {
+          await anonymizedCustomerService.insertBatch(batch);
+          batch = [];
+          await saveResumeToken(resumeToken);
+        }, 1000);
+      }
+    }
+  } catch (error) {
+    console.error('Error processing change stream:', error);
+  } finally {
+    changeStream.close();
+  }
 })();
