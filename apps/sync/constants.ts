f26a4738ce42d757fc2428f8d5a93143f38c2dff
Sergei <dev.sergeialimov@gmail.com>
2023-11-19 16:20:34 +0300

    add comment to constants

--- apps/sync/constants.ts
+++ apps/sync/constants.ts
@@ -1,3 +1,3 @@
-export const RESUME_TOKEN_PATH = "apps/sync/resume-token.txt";
+export const RESUME_TOKEN_PATH = "apps/sync/resume-token.txt"; // should be configurable
 export const BATCH_LENGTH = 1000;
 export const BATCH_INTERVAL = 1000;
