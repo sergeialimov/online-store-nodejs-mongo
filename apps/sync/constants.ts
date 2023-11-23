b343854fac6e77882bcd59cbfcff8b1deb42e6c3
Sergei <dev.sergeialimov@gmail.com>
2023-11-23 16:48:11 +0300

    group constants

--- apps/sync/constants.ts
+++ apps/sync/constants.ts
@@ -1,6 +1,10 @@
 // Configure constants externally for flexibility
+
+// full reindex
+export const FULL_REINDEX_OPTION = "--full-reindex";
+export const SYNC_BATCH_SIZE = 10000;
+
+// realtime
 export const RESUME_TOKEN_PATH = "apps/sync/resume-token.txt";
 export const BATCH_SIZE = 1000;
 export const TIMEOUT_INTERVAL = 1000;
-export const SYNC_BATCH_SIZE = 10000;
-export const FULL_REINDEX_OPTION = "--full-reindex";
